dd9f03f932ba117cc8a03a90f5b9cea555b769fe
cleberfc23 <cleberfc23@gmail.com>
2023-05-31 16:54:30 -0300

    class sorted(); fold(); takeLast()

--- kotlin_arrays_collections_list/src/main/kotlin/tests/testBigDecimal.kt
+++ kotlin_arrays_collections_list/src/main/kotlin/tests/testBigDecimal.kt
@@ -9,13 +9,27 @@ fun testBigDecimal() {
 
     val aumento: BigDecimal = "1.1".toBigDecimal()
     val salarioAumentado = salario.map { salario ->
-        if (salario < "5000.00".toBigDecimal()) salario + "500.00".toBigDecimal()
-        else (salario * aumento).setScale(2, RoundingMode.UP)
+        calculaAumento(salario, aumento)
     }.toTypedArray()
     println(salario.contentToString())
     println(salarioAumentado.contentToString())
+    println("Gasto inicial: R$${salarioAumentado.somatoria()}")
+
+    val meses = "6".toBigDecimal()
+    val gastoSemestral = salarioAumentado.fold(salarioAumentado.somatoria()) { acumulador, salario ->
+        acumulador + (salario * meses).setScale(2, RoundingMode.UP)
+    }
+    println("Gasto semestral: R$${gastoSemestral}")
+
+    val salariosOrdenados = salarioAumentado.sorted()
+    val tresMaioresSalarios = salariosOrdenados.takeLast(3).toTypedArray()
+    println("Media dos tres maiores salarios: R$${tresMaioresSalarios.media()}")
 
 }
+
+private fun calculaAumento(salario: BigDecimal, aumento: BigDecimal) =
+    if (salario < "5000.00".toBigDecimal()) salario + "500.00".toBigDecimal()
+    else (salario * aumento).setScale(2, RoundingMode.UP)
 
 fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
     val array = Array<BigDecimal>(valores.size) { i ->
@@ -24,6 +38,18 @@ fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
     return array
 }
 
-fun aumento() {
+fun Array<BigDecimal>.media(): BigDecimal {
+    return if (this.isNotEmpty()) {
+        (this.somatoria()) / this.size.toBigDecimal()
 
+    } else {
+        BigDecimal.ZERO
+    }
+}
+
+
+fun Array<BigDecimal>.somatoria(): BigDecimal {
+    return this.reduce { acumulador, valor ->
+        acumulador + valor
+    }
 }
